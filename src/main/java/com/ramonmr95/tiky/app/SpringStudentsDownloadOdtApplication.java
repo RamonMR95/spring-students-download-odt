c94fab0a21d893d94d68e8391e798e421af8ad9e
Ramon Moñino <ramonmr16@gmail.com>
2020-01-15 16:15:09 +0100

    test

--- src/main/java/com/ramonmr95/tiky/app/SpringStudentsDownloadOdtApplication.java
+++ src/main/java/com/ramonmr95/tiky/app/SpringStudentsDownloadOdtApplication.java
@@ -2,18 +2,12 @@ package com.ramonmr95.tiky.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringStudentsDownloadOdtApplication extends SpringBootServletInitializer {
+public class SpringStudentsDownloadOdtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudentsDownloadOdtApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringStudentsDownloadOdtApplication.class);
-	}
 }
