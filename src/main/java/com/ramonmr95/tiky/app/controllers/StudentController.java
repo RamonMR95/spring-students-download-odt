af6cecf1befdc9697ee265debbf8643005e35d03
Ramon Moñino <ramonmr16@gmail.com>
2020-01-16 21:48:40 +0100

    Terminado generacion word, falta @valid student en form de registro

--- src/main/java/com/ramonmr95/tiky/app/controllers/StudentController.java
+++ src/main/java/com/ramonmr95/tiky/app/controllers/StudentController.java
@@ -5,12 +5,10 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
-import java.util.Date;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -19,7 +17,6 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
-import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFFooter;
 import org.apache.poi.xwpf.usermodel.XWPFHeader;
@@ -57,7 +54,7 @@ public class StudentController {
 	private IStudentService studentService;
 
 	private String extension;
-
+	
 	@GetMapping({ "/list", "", "/" })
 	public String list(Model model) {
 		List<Student> students = this.studentService.findAll();
@@ -139,74 +136,102 @@ public class StudentController {
 		XWPFHeader header = headerFooterPolicy.createHeader(XWPFHeaderFooterPolicy.DEFAULT);
 
 		paragraph = header.getParagraphArray(0);
-		if (paragraph == null) paragraph = header.createParagraph();
+		
+		if (paragraph == null) 
+			paragraph = header.createParagraph();
+		
 		paragraph.setAlignment(ParagraphAlignment.RIGHT);
 		
 		run = paragraph.createRun();
 		run.setFontSize(14);
 		run.setFontFamily("Times New Roman");
 		run.setBold(true);
-		run.setText("Ramón Moñino Rubio	 -	Antonio Ruiz Marin");
+		run.setText("Ramón Moñino Rubio	 -	Antonio Ruiz Marín");
 		run.addCarriageReturn();
 		run.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-		
 		
 		paragraph = doc.createParagraph();
 		run = paragraph.createRun();
 		
-
-		// convert buffered image to Input Stream
-		
-		BufferedImage screenFullImage;
-		try {
-			screenFullImage = ImageIO.read(new ByteArrayInputStream(student.getPhoto()));
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(screenFullImage, "jpeg", baos);
-			baos.flush();
-			ByteArrayInputStream bis = new ByteArrayInputStream(baos.toByteArray());
-			baos.close();
+		if (student.getPhoto() != null && student.getPhoto().length > 1) {
+	 		BufferedImage imgByte;
+			try {
+				imgByte = ImageIO.read(new ByteArrayInputStream(student.getPhoto()));
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ImageIO.write(imgByte, "jpeg", baos);
+				baos.flush();
+				ByteArrayInputStream bis = new ByteArrayInputStream(baos.toByteArray());
+				baos.close();
+				
+				run.addPicture(bis, XWPFDocument.PICTURE_TYPE_JPEG, "image file", Units.toEMU(130), Units.toEMU(180));
+				bis.close();
+			} 
+			catch (IOException | InvalidFormatException e) {
 			
-			// add image to word doc
+			}
+			
 			run.addBreak();
-			run.addPicture(bis, XWPFDocument.PICTURE_TYPE_JPEG, "image file", Units.toEMU(180), Units.toEMU(150));
-			bis.close();
-
-		} catch (IOException | InvalidFormatException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			paragraph = doc.createParagraph();
 		}
 
-	
 		
-//		// create table in header
-//		paragraph = header.createParagraph();
-//		XmlCursor cursor = paragraph.getCTP().newCursor();
-//		XWPFTable table = header.insertNewTbl(cursor);
-//		XWPFTableRow row = table.getRow(0); if (row == null) row = table.createRow();
-//		int twipsPerInch =  1440;
-//		table.getCTTbl().addNewTblGrid().addNewGridCol().setW(BigInteger.valueOf(6 * twipsPerInch));
-//		for (int i = 0; i < 3; i++) {
-//		XWPFTableCell cell = row.getCell(i); if (cell == null) cell = row.createCell();
-//		CTTblWidth tblWidth = cell.getCTTc().addNewTcPr().addNewTcW();
-//		tblWidth.setW(BigInteger.valueOf(2 * twipsPerInch));
-//		tblWidth.setType(STTblWidth.DXA);
-//		paragraph = cell.getParagraphs().get(0);
-//		run = paragraph.createRun();
-//		run.setText("Header Table Cell " + i);
-//		}
-
-		// create footer start
+		XmlCursor cursor = paragraph.getCTP().newCursor();
+		XWPFTable table = doc.insertNewTbl(cursor);
+		XWPFTableRow firstRow = table.getRow(0);
+		
+		if (firstRow == null)
+			firstRow = table.createRow();
+		
+		int twipsPerInch =  1440;
+		
+		table.getCTTbl().addNewTblGrid().addNewGridCol().setW(BigInteger.valueOf(5 * twipsPerInch));
+		
+		String[] fields = {"Name", "Address", "Town", "ZIP Code"};
+		String[] stValues = {student.getName(), student.getAddress(), student.getTown(), student.getZipCode()};
+		
+		for (int i = 0; i < fields.length; i++) {
+			XWPFTableCell cell = firstRow.getCell(i); 
+			if (cell == null) cell = firstRow.createCell();
+			cell.setColor("33FFFF");
+			
+			CTTblWidth tblWidth = cell.getCTTc().addNewTcPr().addNewTcW();
+			tblWidth.setW(BigInteger.valueOf(2 * twipsPerInch));
+			tblWidth.setType(STTblWidth.DXA);
+			
+			paragraph = cell.getParagraphs().get(0);
+			run = paragraph.createRun();
+			run.setText(fields[i]);
+		}
+		
+		XWPFTableRow tableOneRowTwo = table.createRow();
+	        
+		for (int i = 0; i < stValues.length; i++) {
+			XWPFTableCell cell = tableOneRowTwo.getCell(i); 
+			if (cell == null) cell = tableOneRowTwo.createCell();
+			
+			CTTblWidth tblWidth = cell.getCTTc().addNewTcPr().addNewTcW();
+			tblWidth.setW(BigInteger.valueOf(2 * twipsPerInch));
+			tblWidth.setType(STTblWidth.DXA);
+			
+			paragraph = cell.getParagraphs().get(0);
+			run = paragraph.createRun();
+			run.setText(stValues[i]);
+		}
+		
 		XWPFFooter footer = headerFooterPolicy.createFooter(XWPFHeaderFooterPolicy.DEFAULT);
 
 		paragraph = footer.getParagraphArray(0);
-		if (paragraph == null) paragraph = footer.createParagraph();
+		
+		if (paragraph == null) 
+			paragraph = footer.createParagraph();
+		
 		paragraph.setAlignment(ParagraphAlignment.CENTER);
 
 		run = paragraph.createRun();
-		run.setText("The Footer:");
+		run.setText("Proyecto creado por Ramón Moñino Rubio y Antonio Ruiz Marín © 2020-21");
 
 		try {
-			doc.write(new FileOutputStream("CreateWordHeaderFooterTable.docx"));
+			doc.write(new FileOutputStream(student.getName() + ".docx"));
 			doc.close();
 		} catch (IOException e) {
 			return "redirect:/student?id=" + id;
