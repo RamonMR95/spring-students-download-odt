739794ecc87bfd361082df8c652796d50a44d2ee
Ramon Moñino <ramonmr16@gmail.com>
2020-01-26 16:45:43 +0100

    Eliminado bin/

--- bin/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO students(name, address, town, zipcode, photo) VALUES ("Ramón", "C/ Falsa", "Algezares", "30157", 0);
-INSERT INTO students(name, address, town, zipcode, photo) VALUES ("Antonio", "C/ Los dolores", "Murcia", "30011", 0);
-INSERT INTO students(name, address, town, zipcode, photo) VALUES ("Satán", "C/ Del diablo", "Infierno", "66666", 0);
